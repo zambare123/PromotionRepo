e6e1f602ef799de5152f2bdeb2846ec73eda3454
zambare123 <zambare123@gmail.com>
2020-07-30 21:19:05 +0530

    added product price columns and updated database

---
+++ Migrations/202007301541088_SKU21.cs
@@ -0,0 +1,20 @@
+﻿namespace SKUPromotions.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SKU21 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.SKUProductsDetails", "ProductPrice", c => c.Double(nullable: false));
+            AlterColumn("dbo.SKUProductsDetails", "SKUProductNm", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.SKUProductsDetails", "SKUProductNm", c => c.Int(nullable: false));
+            DropColumn("dbo.SKUProductsDetails", "ProductPrice");
+        }
+    }
+}
