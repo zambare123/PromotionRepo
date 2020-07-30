e974541a5b8872ec203bb68ef3db5f9b200893b8
zambare123 <zambare123@gmail.com>
2020-07-30 21:06:27 +0530

    addin code base
    addin code base

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKUPromotions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string IsContinue = string.Empty;
+            do
+            {
+                Console.WriteLine("Please enter Product name: ");
+                string productvalue = Console.ReadLine();
+                Console.WriteLine("Please enter Product Number: ");
+                int productnumber = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("do you Want to Conitunue(y/n) : ");
+                IsContinue = Console.ReadLine();
+            }
+            while (IsContinue == "y");
+        }
+    }
+}
