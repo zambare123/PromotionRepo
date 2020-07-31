5c94039072943273540ed472d875ad5bf68430ce
zambare123 <zambare123@gmail.com>
2020-07-31 11:40:19 +0530

    added DB Scripts and big fixes

---
+++ DBScripts/SKUProductsDetails_Insert.sql
@@ -0,0 +1,11 @@
+USE [SKUShipping]
+GO
+
+INSERT INTO [dbo].[SKUProductsDetails] ([SKUProductNm] ,[CreatedBy] ,[UpdatedBy]  ,[CreatedDt] ,[UpdatedDt] ,[MachineNm],[ProductPrice]) VALUES ('A' ,'Mohan Zambare' ,'Mohan Zambare' ,GETDATE()  ,GETDATE()  ,'System' ,50.0)
+INSERT INTO [dbo].[SKUProductsDetails] ([SKUProductNm] ,[CreatedBy] ,[UpdatedBy]  ,[CreatedDt] ,[UpdatedDt] ,[MachineNm],[ProductPrice]) VALUES ('B' ,'Mohan Zambare' ,'Mohan Zambare' ,GETDATE()  ,GETDATE()  ,'System' ,30.0)
+INSERT INTO [dbo].[SKUProductsDetails] ([SKUProductNm] ,[CreatedBy] ,[UpdatedBy]  ,[CreatedDt] ,[UpdatedDt] ,[MachineNm],[ProductPrice]) VALUES ('C' ,'Mohan Zambare' ,'Mohan Zambare' ,GETDATE()  ,GETDATE()  ,'System' ,20.0)
+INSERT INTO [dbo].[SKUProductsDetails] ([SKUProductNm] ,[CreatedBy] ,[UpdatedBy]  ,[CreatedDt] ,[UpdatedDt] ,[MachineNm],[ProductPrice]) VALUES ('D' ,'Mohan Zambare' ,'Mohan Zambare' ,GETDATE()  ,GETDATE()  ,'System' ,15.0)
+
+GO
+
+
